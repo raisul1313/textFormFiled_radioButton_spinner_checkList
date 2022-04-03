fb7fc19b75192f0bc61b06345dee79c2a16d5d6f
raisul1313 <raisul.islam1313@gmail.com>
2022-04-03 16:15:19 +0600

    added some changes!

--- lib/view/input_information_page.dart
+++ lib/view/input_information_page.dart
@@ -10,7 +10,9 @@ class InputInformationFiled extends StatefulWidget {
 
 class _InputInformationFiledState extends State<InputInformationFiled> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  var _groupValue = 1;
   bool _isValid = false;
+  bool _isChecked = false;
 
   void _saveForm() {
     setState(() {
@@ -27,71 +29,87 @@ class _InputInformationFiledState extends State<InputInformationFiled> {
           "Owner Information",
         ),
         centerTitle: true,
-        backgroundColor: Colors.purple,
+        backgroundColor: Theme.of(context).primaryColor,
       ),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "*All Fields Are Required.",
-                    style: TextStyle(
-                      color: Colors.red,
-                      fontWeight: FontWeight.bold,
+          child: Container(
+            height: size.height,
+            width: size.width,
+            color: Colors.white70,
+            child: Form(
+              key: _formKey,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "*All Fields Are Required.",
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Center(
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: Theme.of(context).primaryColor),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    Center(
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Theme.of(context).primaryColor),
+                              ),
+                              hintText: "Enter Your Name",
+                              labelText: "Name",
+                              prefixIcon: Icon(Icons.person),
                             ),
-                            hintText: "Enter Your Name",
-                            labelText: "Name",
-                            prefixIcon: Icon(Icons.person),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'This field is required';
+                              } else if (!RegExp(
+                                      r'^[A-Za-z.-]+(\s*[A-Za-z.-]+)*$')
+                                  .hasMatch(value)) {
+                                return "Type a valid name";
+                              }
+                              return null;
+                            },
+                            textInputAction: TextInputAction.next,
                           ),
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        TextFormField(
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: Theme.of(context).primaryColor),
+                          SizedBox(
+                            height: 30.0,
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Theme.of(context).primaryColor),
+                              ),
+                              hintText: "Enter Your Email Address",
+                              labelText: "Email Address",
+                              prefixIcon: Icon(Icons.email_outlined),
                             ),
-                            hintText: "Enter Your Email Address",
-                            labelText: "Email Address",
-                            prefixIcon: Icon(Icons.email_outlined),
+                            validator: (String? value) {
+                              if (value!.isEmpty) {
+                                return 'This field is required';
+                              } else if (!RegExp(
+                                      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+                                  .hasMatch(value)) {
+                                return "Please enter a valid email address";
+                              }
+                              return null;
+                            },
+                            textInputAction: TextInputAction.next,
                           ),
-                          validator: (String? value) {
-                            if (value == null || value.isEmpty) {
-                              return 'This field is required';
-                            } else if (!RegExp(
-                                    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
-                                .hasMatch(value)) {
-                              return "Please enter a valid email address";
-                            }
-                            return null;
-                          },
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        TextFormField(
+                          SizedBox(
+                            height: 30.0,
+                          ),
+                          TextFormField(
                             decoration: InputDecoration(
                               border: OutlineInputBorder(),
                               focusedBorder: OutlineInputBorder(
@@ -101,64 +119,209 @@ class _InputInformationFiledState extends State<InputInformationFiled> {
                               hintText: "Enter Your Phone Number",
                               labelText: "Phone Number",
                               prefixIcon: Icon(Icons.phone_android),
+                              prefixText: "+88 ",
                             ),
                             keyboardType: TextInputType.number,
                             inputFormatters: [
                               FilteringTextInputFormatter.digitsOnly,
+                              LengthLimitingTextInputFormatter(11),
                             ],
-                            validator: (String? value) {
-                              if (value == null || value.isEmpty) {
+                            validator: (value) {
+                              if (value!.isEmpty) {
                                 return 'This field is required';
-                              } else if (value.length != 11) {
-                                return 'Number must be 11 digit';
-                              } else if (!RegExp(r'(^01[13-9]\d{11}$)')
+                              } else if (!RegExp(r'^01[3-9]\d{8}$')
                                   .hasMatch(value)) {
                                 return 'Please enter valid mobile number';
                               }
                               return null;
-                            }),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        TextFormField(
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: Theme.of(context).primaryColor),
-                            ),
-                            hintText: "Enter Your Height",
-                            labelText: "Height (inch)",
-                            prefixIcon: Icon(Icons.person),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        TextFormField(
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: Theme.of(context).primaryColor),
-                            ),
-                            hintText: "Enter Your Age",
-                            labelText: "Age",
-                            prefixIcon: Icon(Icons.accessibility_new),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        ElevatedButton(
-                            onPressed: () {
-                              if (_formKey.currentState!.validate()) {}
                             },
-                            child: Text("Submit"))
-                      ],
+                            textInputAction: TextInputAction.next,
+                          ),
+                          SizedBox(
+                            height: 30.0,
+                          ),
+                          Row(
+                            children: [
+                              Expanded(
+                                flex: 1,
+                                child: TextFormField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color:
+                                              Theme.of(context).primaryColor),
+                                    ),
+                                    hintText: "Age",
+                                    labelText: "Age",
+                                    prefixIcon: Icon(Icons.accessibility_new),
+                                  ),
+                                  validator: (value) {
+                                    if (value!.isEmpty) {
+                                      return 'This field is required';
+                                    } else if (int.tryParse(value)! > 200) {
+                                      return "Age between 0 to 200 ";
+                                    }
+                                    return null;
+                                  },
+                                  textInputAction: TextInputAction.done,
+                                  keyboardType: TextInputType.number,
+                                  inputFormatters: [
+                                    FilteringTextInputFormatter.digitsOnly,
+                                    LengthLimitingTextInputFormatter(3),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(
+                                width: 5.0,
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: TextFormField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color:
+                                              Theme.of(context).primaryColor),
+                                    ),
+                                    hintText: "Enter Your Height",
+                                    labelText: "Height (feet-inch)",
+                                    prefixIcon: Icon(Icons.height),
+                                  ),
+                                  validator: (value) {
+                                    if (value!.isEmpty) {
+                                      return 'This field is required';
+                                    } else if (double.tryParse(value)! > 8.0) {
+                                      return "Not more than 8 feet ";
+                                    }
+                                    return null;
+                                  },
+                                  textInputAction: TextInputAction.done,
+                                  keyboardType: TextInputType.number,
+                                  inputFormatters: [
+                                    //FilteringTextInputFormatter.digitsOnly,
+                                    LengthLimitingTextInputFormatter(3),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                "Gender: ",
+                                style: TextStyle(
+                                  color: Theme.of(context).primaryColor,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Radio(
+                                  value: 1,
+                                  groupValue: _groupValue,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      _groupValue = 1;
+                                    });
+                                  }),
+                              Text("Male"),
+                              Radio(
+                                  value: 2,
+                                  groupValue: _groupValue,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      _groupValue = 2;
+                                    });
+                                  }),
+                              Text("Female"),
+                              Radio(
+                                  value: 3,
+                                  groupValue: _groupValue,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      _groupValue = 3;
+                                    });
+                                  }),
+                              Text("Other"),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+
+
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Owner Type: ",
+                                style: TextStyle(
+                                  color: Theme.of(context).primaryColor,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Checkbox(
+                                          value: _isChecked,
+                                          onChanged: (bool? value) {
+                                            setState(() {
+                                              _isChecked = value!;
+                                            });
+                                          }),
+                                      Text("Bike"),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Checkbox(
+                                          value: _isChecked,
+                                          onChanged: (bool? value) {
+                                            setState(() {
+                                              _isChecked = value!;
+                                            });
+                                          }),
+                                      Text("Car"),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Checkbox(
+                                          value: _isChecked,
+                                          onChanged: (bool? value) {
+                                            setState(() {
+                                              _isChecked = value!;
+                                            });
+                                          }),
+                                      Text("Truck"),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+
+
+
+
+
+
+                          ElevatedButton(
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {}
+                              },
+                              child: Text("Submit"))
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
